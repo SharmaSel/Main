53b301db85e135cf3038b381983bb6aa3d9cd31c
sharm <sharm@10.0.0.171>
2023-01-20 21:13:21 -0500

    step1

---
+++ sel/src/main/java/Pages/HelperClass.java
@@ -0,0 +1,19 @@
+package Pages;
+
+
+	
+	import org.openqa.selenium.WebDriver;
+
+	public class HelperClass {
+
+		WebDriver driver;
+		public HelperClass(WebDriver driver)
+		{
+			this.driver=driver;
+		}
+		public void launchBrowser(String url)
+		{
+			driver.manage().window().maximize();
+			driver.get(url );
+		}
+}
